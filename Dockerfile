54f4d91da8563fa0175613dbf8d971859fa04fad
Алан Джелиев <djalan84@mail.ru>
2023-11-26 17:53:44 +0300

    Refactoring app.py - Root user creation script added - Password set script added - BD creation script added - Table creation script added - Table population script added - Delete database and table scripts added
    Refactoring index.html
    Refactoring docker-compose.yaml
    
    Freezing new dependencies in requirements.txt
    
    Added .dockerignore file
    Added .gitignore file

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
+
 FROM python:3.9
 
 WORKDIR /app
 
-COPY requirements.txt /app
+COPY requirements.txt .
+
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . /app
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
 
-EXPOSE 8000
+COPY . .
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -4,38 +4,70 @@ import pymysql
 
 app = Flask(__name__)
 
-class Database:
-    def __init__(self):
-        host = "127.0.0.1"
-        user = "root"
-        password = "12081984zZ"
-        db = "test_aws"
-        self.con = pymysql.connect(
-            host=host,
-            user=user,
-            password=password,
-            db=db,
-            cursorclass=pymysql.cursors.DictCursor
-        )
-        self.cur = self.con.cursor()
+# Установка соединения с базой данных
+conn = pymysql.connect(
+    host='db',  # Название сервиса контейнера базы данных в Docker Compose
+    user='root',
+    password='Demid2019Mark2022'
+)
 
-    def list_users(self):
-        self.cur.execute("SELECT * FROM users")
-        result = self.cur.fetchall()
-        return result
+# Создаем базу данных
+create_database_query = 'CREATE DATABASE your_database;'
+with conn.cursor() as cursor:
+    drop_database_query = "DROP DATABASE your_database;"
 
+    # Execute the DROP DATABASE query
+    cursor.execute(drop_database_query)
+
+    # Commit the changes
+    conn.commit()
+    cursor.execute(create_database_query)
+
+    # Создаем пользователя root с паролем
+    create_user_query = "CREATE USER 'root'@'db' IDENTIFIED BY 'Demid2019Mark2022';"
+    grant_query = "GRANT ALL PRIVILEGES ON your_database.* TO 'root'@'db';"
+    cursor.execute(create_user_query)
+    cursor.execute(grant_query)
+
+    drop_table_query = 'DROP TABLE IF EXISTS good;'
+    cursor.execute(drop_table_query)
+    conn.commit()
+
+    # Создание таблицы
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS `good` (
+      `id` int(11) NOT NULL,
+      `category_id` int(11) NOT NULL,
+      `name` varchar(255) DEFAULT NULL,
+      `count` int(11) NOT NULL,
+      `price` int(11) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    """
+
+    cursor.execute(create_table_query)
+
+    # Добавление данных в таблицу
+    insert_data_query = """
+    INSERT INTO `good` (`id`, `category_id`, `name`, `count`, `price`) VALUES
+    (1, 3, 'Айс Ти «Инжирный персик»', 201, 157),
+    (2, 3, 'Айс Ти «Черничный Прованс»', 201, 157),
+    (3, 3, 'Айс Ти «Сочное манго»', 288, 157);
+    """
+
+    cursor.execute(insert_data_query)
+
+    # Фиксация изменений
+    conn.commit()
 
 @app.route('/')
 def index():
-    def db_query():
-        db = Database()
-        users = db.list_users()
-        return users
+    # Выполнение запроса на получение всех данных из таблицы
+    select_data_query = "SELECT * FROM `good`"
+    cursor.execute(select_data_query)
+    results = cursor.fetchall()
 
-    res = db_query()
+    return render_template('index.html', results=results)
 
-    return render_template("index.html", result=res)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
